8b6cc4d98703985f2c01f757d292da04db2e508d
grryv <grryv@grry>
2020-10-21 19:49:56 +0700

    Tugas - Latihan Pertemuan 1 Garry Prang 10116147 PBO-2

---
+++ src/pbo2/pkg10116147/latihan16/penugasan/PBO210116147Latihan16Penugasan.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pbo2.pkg10116147.latihan16.penugasan;
+
+/**
+ * @author
+ * Nama                 : Garry Prang
+ * NIM                  : 10116147
+ * Kelas                : PBO-2
+ * Deskripsi Program    : Program untuk mengimplementasikan operator logika.
+ */
+
+public class PBO210116147Latihan16Penugasan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 10;
+        int b = 20;
+        int c = 0;
+        
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Nilai c = " + c+"\n");
+        
+        c = a + b;
+        System.out.println("c = a + b = " + c);
+        c += a;
+        System.out.println("c += a = " + c);
+        c -= a;
+        System.out.println("c -= a = " + c);
+        c *= a;
+        System.out.println("c *= a = " + c);
+        a = 10;
+        c = 15;
+        c /= a;
+        System.out.println("c = /a = " + c);
+        a = 10;
+        c = 15;
+        c %= a;
+        System.out.println("c = %a = " + c);
+        c <<= 2;
+        System.out.println("c <<= 2 = " + c);
+        c >>= 2;
+        System.out.println("c >>= 2 = " + c);
+        c >>= 2;
+        System.out.println("c >>= a = " + c);
+        c &= a;
+        System.out.println("c =& 2 = " + c);
+        c ^= a;
+        System.out.println("c =^ a = " + c);
+        c |= a;
+        System.out.println("c =| a = " + c);
+    }
+    
+}
